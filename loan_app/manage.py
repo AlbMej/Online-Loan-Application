c55d63aef77accaaf4962fa6c0118d94d0da472f
Alberto Mejia <albertomejia295@gmail.com>
2019-05-03 09:35:05 -0400

    Fix wsgi path

--- loan_app/manage.py
+++ loan_app/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loan_app.settings.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loan_app.settings.heroku')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
