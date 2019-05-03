5586c0f7076d28e96107adcb1fe0a95c5ae0b296
Alberto Mejia <albertomejia295@gmail.com>
2019-05-02 20:40:05 -0400

    Setup Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn loan_app.loan_app.wsgi --log-file -
--- loan_app/loan_app/wsgi.py
+++ loan_app/loan_app/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loan_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loan_app.settings.heroku')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
