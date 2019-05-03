0e8388c9c9b9bf45242aab4cf6349c8bf1d7fb59
Alberto Mejia <albertomejia295@gmail.com>
2019-05-03 04:27:37 -0400

    Add application attribute

--- loan_app/loan_app/wsgi.py
+++ loan_app/loan_app/wsgi.py
@@ -14,5 +14,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loan_app.settings.heroku')
 
-#application = get_wsgi_application()
+application = get_wsgi_application()
 #application = DjangoWhiteNoise(application)
